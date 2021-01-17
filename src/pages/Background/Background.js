6aa1ee03f89340d09d3ba8a3237dade86fc19e7c
RUD20LLE <ruddy20calle@gmail.com>
2021-01-16 23:14:48 -0400

    controlPanel2.0

---
+++ src/pages/Background/Background.js
@@ -0,0 +1,1 @@
+import React from 'react';import './Background.css';const Background = () => {    const imagePath = process.env.PUBLIC_URL + '/Background.png';    return (        <div className="background-container">            <img src={imagePath} alt="Background" className="background-image" />        </div>    );}export default Background;
